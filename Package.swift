54ba728b53d35c6e02fd04457e15bb19c45249a5
Sam Deane <sam@elegantchaos.com>
2019-06-17 12:18:21 +0100

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "CommandShell", targets: ["CommandShell"])
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Arguments.git", from: "1.0.7"),
+        .package(url: "https://github.com/elegantchaos/Arguments.git", from: "1.0.8"),
     ],
     targets: [
         .target(
