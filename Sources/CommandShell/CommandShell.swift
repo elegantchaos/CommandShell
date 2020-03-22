2152043bb26dc82611d120e40ead72ac04718cb6
Sam Deane <sam@elegantchaos.com>
2020-03-22 18:53:47 +0000

    Handle loading of embedded plist.

--- Sources/CommandShell/CommandShell.swift
+++ Sources/CommandShell/CommandShell.swift
@@ -14,7 +14,9 @@ struct CommandShell: ParsableCommand {
         defaultSubcommand: nil
     )
     
-    static var version = SemanticVersion(1)
+    static var info = loadInfoPlist()
+    static var version = loadVersion()
+    static var buildNumber = loadBuildNumber()
     
     @Flag(help: "Show the version.") var version: Bool
 
@@ -26,9 +28,51 @@ struct CommandShell: ParsableCommand {
         }
     }
     
-    var name: String {
+    static var executable: String {
         let url = URL(fileURLWithPath: CommandLine.arguments[0])
         return url.lastPathComponent
 
     }
+    
+    static var name: String {
+        return info["CFBundleDisplayName"] ?? executable
+    }
+    
+    static func loadVersion() -> SemanticVersion {
+        if let string = info["CFBundleShortVersionString"], let version = SemanticVersion(string) {
+            return version
+        } else {
+            return SemanticVersion(1)
+        }
+    }
+    
+    static func loadBuildNumber() -> Int {
+        if let string = info["CFBundleVersion"], let build = Int(argument: string) {
+            return build
+        } else {
+            return 0
+        }
+    }
+    static func loadInfoPlist() -> [String:String] {
+        #if os(macOS) || os(iOS)
+        if let handle = dlopen(nil, RTLD_LAZY) {
+            defer { dlclose(handle) }
+            
+            if let ptr = dlsym(handle, MH_EXECUTE_SYM) {
+                var size: UInt = 0
+                let mhExecHeaderPtr = ptr.assumingMemoryBound(to: mach_header_64.self)
+                if let ptr = getsectiondata(mhExecHeaderPtr, "__TEXT", "__Info_plist", &size) {
+                    let data = Data(bytesNoCopy: ptr, count: Int(size), deallocator: .none)
+                    do {
+                        let info = try PropertyListSerialization.propertyList(from: data, options: [], format: nil)
+                        return info as? [String:String] ?? [:]
+                    } catch {
+                    }
+                }
+            }
+        }
+        #endif
+        return [:]
+    }
+
 }
