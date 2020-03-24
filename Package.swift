9bee79f8d50b1e7f588135ec42bc711b64697840
Sam Deane <sam@elegantchaos.com>
2020-03-24 14:46:20 +0000

    Updated semantic version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.4"),
         .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.5.3"),
-        .package(url: "https://github.com/elegantchaos/SemanticVersion.git", from: "1.0.2"),
+        .package(url: "https://github.com/elegantchaos/SemanticVersion.git", from: "1.0.3"),
         .package(url: "https://github.com/elegantchaos/XCTestExtensions.git", from: "1.1.0"),
     ],
     targets: [
